f203bd314d73056b104796046406120b24956481
Axel Ivanda Tanjung <axel.tanjung@gmail.com>
2024-12-18 14:53:23 +0700

    Pulling multiple values at once

--- dags/x_com_dag.py
+++ dags/x_com_dag.py
@@ -8,10 +8,14 @@ with DAG('x_com_dag', start_date = datetime(2022, 1, 1), schedule_inverval = '@d
     @task
     def peter_task(ti=None):
         ti.xcom_push(key = 'peter_task', value = 'iphone')
+    
+    @task
+    def lorie_task(ti=None):
+        ti.xcom_push(key = 'lorie_task', value = 'samsung')
 
     @task
     def bryan_task(ti=None):
-        phone = ti.xcom_pull(task_ids = 'peter_task', key = 'mobile_phone')
+        phone = ti.xcom_pull(task_ids = ['peter_task', 'lorie_task'], key = 'mobile_phone')
         print(phone)
 
-    peter_task >> bryan_task()
+    peter_task >> lorie_task >> bryan_task
